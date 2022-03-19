81310aa259df498544384ec6bb4de7e15e21fa5b
뱀 <54323618+viyamKUN@users.noreply.github.com>
2022-03-20 01:02:24 +0900

    그냥 사용하던 값을 상수로 교체함 (#7)
    * fixed #4

--- src/app/app_render.cpp
+++ src/app/app_render.cpp
@@ -8,17 +8,18 @@ using namespace renderer;
 namespace game {
 
 bool App::InitSurfaces() {
-  if ((display = SDL_SetVideoMode(600, 600, 32,
+  auto width = GridSize * GridCount;
+  if ((display = SDL_SetVideoMode(width, width, 32,
                                   SDL_HWSURFACE | SDL_DOUBLEBUF)) == NULL) {
     return false;
   }
-  if ((grid_image = Surface::OnLoad("../assets/grid.bmp")) == NULL) {
+  if ((grid_image = Surface::OnLoad(GetFileDir("grid.bmp"))) == NULL) {
     return false;
   }
-  if ((x_image = Surface::OnLoad("../assets/x.bmp")) == NULL) {
+  if ((x_image = Surface::OnLoad(GetFileDir("x.bmp"))) == NULL) {
     return false;
   }
-  if ((o_image = Surface::OnLoad("../assets/o.bmp")) == NULL) {
+  if ((o_image = Surface::OnLoad(GetFileDir("o.bmp"))) == NULL) {
     return false;
   }
   Surface::Transparent(x_image, 255, 255, 255);
@@ -30,9 +31,10 @@ bool App::InitSurfaces() {
 void App::OnRender() {
   Surface::OnDraw(display, grid_image, 0, 0);
 
-  for (int i = 0; i < 9; i++) {
-    int x = (i % 3) * 200;
-    int y = (i / 3) * 200;
+  int whole_grid_count = GridCount * GridCount;
+  for (int i = 0; i < whole_grid_count; i++) {
+    int x = (i % GridCount) * GridSize;
+    int y = (i / GridCount) * GridSize;
     if (grid[i] == GRID_TYPE_X) {
       Surface::OnDraw(display, x_image, x, y);
     } else if (grid[i] == GRID_TYPE_O) {
@@ -40,6 +42,13 @@ void App::OnRender() {
     }
   }
   SDL_Flip(display);
+}
+
+const char* App::GetFileDir(const char* fileName) {
+  char* path = new char[strlen(AssetPath) + strlen(fileName)];
+  strcat(path, AssetPath);
+  strcat(path, fileName);
+  return path;
 }
 
 }  // namespace game
